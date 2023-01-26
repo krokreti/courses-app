2d047c82eadba6456da7f9a4338c231120091640
davi <davipinheiro1993@gmail.com>
2023-01-26 11:49:54 -0300

    Add two way databinding to login screen

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,8 +1,12 @@
 <template>
   <div>
-    Login screen
-    <Input :floatingLabel="'Login'" :inputType="'text'" v-model="form.login" :required="true"/>
-    <Input :floatingLabel="'Password'" :inputType="'password'" v-model="form.password" :required="true"/>
+    <div class="login-container" >
+        <Input :floatingLabel="'Login'" :inputType="'text'" v-model="form.login" :required="true"/>
+        <Input :floatingLabel="'Password'" :inputType="'password'" v-model="form.password" :required="true"/>
+        <button type="button" class="btn btn-primary btn-lg">Login</button>
+        <span>Don't have an account yet?</span>
+        <span>Register now!</span>
+    </div>
   </div>
 </template>
 
@@ -26,5 +30,17 @@ export default defineComponent({
 </script>
 
 <style>
+.login-container {
+    display: flex;
+    flex-direction: column;
+    padding: 2em;
+    margin: 2em;
+    gap: 10px;
+    border:1px solid #969494;
+    border-radius: 5px;
+}
 
+.login-container span {
+    text-align: center;
+}
 </style>
